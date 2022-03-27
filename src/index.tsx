3a783de98c496a370f3aa946152b22e7a03a9851
Eature <781746820@qq.com>
2022-03-28 02:57:13 +0800

    调整

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 import { loadDevTools } from 'jira-dev-tool'
 import { AppProviders } from 'context'
 import 'antd/dist/antd.less'
@@ -21,4 +21,4 @@ loadDevTools(() => (
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
