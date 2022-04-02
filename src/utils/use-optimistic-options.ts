45f553a4458f60016b5158c4f6e16e216372b75c
Eature <781746820@qq.com>
2022-04-02 15:48:54 +0800

    1

---
+++ src/utils/use-optimistic-options.ts
@@ -0,0 +1,24 @@
+import { Query, QueryKey, useQueryClient } from 'react-query'
+
+export const useConfig = (queryKey: QueryKey, callBack: (target: any, old?: any[]) => any[]) => {
+    const queryClient = useQueryClient()
+
+    return {
+        onSuccess: () => queryClient.invalidateQueries(queryKey),
+        onError(error: any, newItem: any, context: any) {
+            queryClient.setQueryData(queryKey, context.previousItems)
+        },
+        async onMutate(target: any) {
+            const previousItems = queryClient.getQueryData(queryKey)
+            queryClient.setQueryData(queryKey, (old?: any[]) => {
+                return callBack(target, old)
+            })
+            return { previousItems }
+        }
+
+    }
+}
+
+export const useDeleteConfig = (queryKey: QueryKey) => useConfig(queryKey, (target, old: any[] | undefined) => old?.filter((item) => item.id !== target.id) || [])
+export const useEditConfig = (queryKey: QueryKey) => useConfig(queryKey, (target, old: any[] | undefined) => old?.map((item) => item.id === target.id ? { ...item, ...target } : item) || [])
+export const useAddConfig = (queryKey: QueryKey) => useConfig(queryKey, (target, old: any[] | undefined) => old ? [...old, target] : [])
